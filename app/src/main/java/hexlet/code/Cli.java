cbc93462542e65ef3dc5f500a3fa2335b0b39301
fugu <ffqwerty3@gmail.com>
2024-05-07 21:35:48 +0300

    fix cli messages

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -8,7 +8,7 @@ public class Cli {
 
         System.out.print("May I have your name? ");
         String userName = scanner.next();
-        System.out.println("Hello, " + userName + "!");
+        System.out.println("Hello, " + userName);
 
         return userName;
     }
