98e433ab94ab3c01edeedc2b321b6006ffcdf9cd
fugu <ffqwerty3@gmail.com>
2024-05-07 21:30:35 +0300

    fix cli messages

--- app/src/main/java/hexlet/code/Engine.java
+++ app/src/main/java/hexlet/code/Engine.java
@@ -26,7 +26,7 @@ public class Engine {
         System.out.println("1 - Greet");
         System.out.println("2- Even");
         System.out.println("3 - Calc");
-        System.out.println("4 - Gcd");
+        System.out.println("4 - GCD");
         System.out.println("5 - Progression");
         System.out.println("6 - Prime");
         System.out.println("0 - Exit");
