8abc39dd2c7ed602f66d48f5b013ad7b7f6b1fa4
fugu <ffqwerty3@gmail.com>
2024-05-16 19:20:50 +0300

    refactor App class

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -10,7 +10,17 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-        showGames();
+        System.out.print("""
+                Please enter the game number and press Enter.
+                1 - Greet
+                2 - Even
+                3 - Calc
+                4 - GCD
+                5 - Progression
+                6 - Prime
+                0 - Exit
+                Your choice:\s"""
+        );
 
         Scanner scanner = new Scanner(System.in);
         String gameCode = scanner.next();
@@ -18,47 +28,41 @@ public class App {
         System.out.println();
         System.out.println("Welcome to the Brain Games!");
 
-        startGames(gameCode);
-    }
-
-    private static void showGames() {
-        System.out.println("Please enter the game number and press Enter.");
-        System.out.println("1 - Greet");
-        System.out.println("2- Even");
-        System.out.println("3 - Calc");
-        System.out.println("4 - GCD");
-        System.out.println("5 - Progression");
-        System.out.println("6 - Prime");
-        System.out.println("0 - Exit");
-        System.out.print("Your choice: ");
-    }
-
-    private static void startGames(String gameCode) {
         switch (gameCode) {
             case "0":
                 System.out.println("See you next time!");
                 break;
             case "1":
-                Cli.askName();
+                askName();
                 break;
             case "2":
-                EvenGame.startEvenGame(Cli.askName());
+                EvenGame.startEvenGame(askName());
                 break;
             case "3":
-                CalcGame.startCalcGame(Cli.askName());
+                CalcGame.startCalcGame(askName());
                 break;
             case "4":
-                GcdGame.startGcdGame(Cli.askName());
+                GcdGame.startGcdGame(askName());
                 break;
             case "5":
-                ProgressionGame.startProgressionGame(Cli.askName());
+                ProgressionGame.startProgressionGame(askName());
                 break;
             case "6":
-                PrimeGame.startPrimeGame(Cli.askName());
+                PrimeGame.startPrimeGame(askName());
                 break;
             default:
                 System.out.println("Game doesn't exist. Bye");
                 break;
         }
+    }
+
+    public static String askName() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("May I have your name? ");
+        String userName = scanner.next();
+        System.out.println("Hello, " + userName +  "!");
+
+        return userName;
     }
 }
